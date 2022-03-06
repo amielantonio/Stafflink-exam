1ed92fd3ead2f9b2ad8550a2662d70441f387364
Amiel Antonio <amielnicole.antonio@gmail.com>
2022-03-07 04:38:14 +0800

    fix frontpage

--- src/js/components/Views/SearchBar.vue
+++ src/js/components/Views/SearchBar.vue
@@ -1,14 +1,24 @@
 <template>
 
-    <form action="http/localhost/omdbapi/search-results" method="GET">
-        <div class="ombd-search">
-            <div class="form-group">
-                <label for="omdb-search__input">Search a Movie: </label>
-                <input type="text" class="omdb-search__input" id="omdb-search__input" name="sr">
-                <button type="submit" class="omdb-search__button btn btn-primary">Search</button>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 offset-sm-2">
+                <form action="http/localhost/omdbapi/search-results" method="GET">
+                    <div class="ombd-search">
+                        <div class="input-group" >
+                            <input type="text" class="omdb-search__input form-control" id="omdb-search__input" name="sr" aria-label="Search"
+                                placeholder="Search a movie. eg: Red, Yellow, Blue">
+                            <button type="submit" class="omdb-search__button btn btn-primary input-group-append">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </div>
+
+                    </div>
+                </form>
             </div>
         </div>
-    </form>
+
+    </div>
 
 </template>
 <script>
